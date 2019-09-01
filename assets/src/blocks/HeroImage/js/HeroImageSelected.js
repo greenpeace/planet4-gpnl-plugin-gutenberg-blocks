cde5751fed8afef4afb6fdf3cde0c6c7bcb268ca
Dirk <d.j.faber@outlook.com>
2019-09-01 18:58:46 +0200

    improve layout of editor buttons

--- assets/src/blocks/HeroImage/js/HeroImageSelected.js
+++ assets/src/blocks/HeroImage/js/HeroImageSelected.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { RichText, MediaUpload, MediaUploadCheck, InspectorControls, BlockControls} from "@wordpress/editor";
-import { Button, PanelBody, ToggleControl } from '@wordpress/components';
+import React, {Component} from 'react';
+import {RichText, MediaUpload, MediaUploadCheck, InspectorControls, BlockControls} from "@wordpress/editor";
+import {Button, PanelBody, ToggleControl, ToolbarButton} from '@wordpress/components';
 
-export default function HeroImageSelected(props) {
+export default class HeroImageSelected extends Component {
 
-  const {
+  render() {
+
+    const {
       title,
       description,
       link_text,
@@ -14,80 +16,83 @@ export default function HeroImageSelected(props) {
       is_small,
       onValueChange,
       onSelectImage,
-    } = props;
+    } = this.props;
 
-  const fields =
-    <div className="page-template hero__wrapper ">
-      <div className="hero__text">
-        <h2>
+
+    const fields =
+      <div className="page-template hero__wrapper ">
+        <div className="hero__text">
+          <h2>
+            <RichText
+              onChange={onValueChange.bind('title')}
+              value={title}
+              tagName={'span'}
+              className={'hero__title'}
+              placeholder={'enter a title (optional)'}
+            />
+          </h2>
           <RichText
-            onChange={onValueChange.bind('title')}
-            value={title}
-            tagName={'span'}
-            className={'hero__title'}
-            placeholder={'enter a title (optional)'}
-          />
-        </h2>
-        <RichText
-          onChange={onValueChange.bind('description')}
-          value={description}
-          tagName={'p'}
-          className={'hero__description'}
-          placeholder={'enter an abstract / description (optional)'}
-        />
-        <div style={{width: '280px'}}>
-          <RichText
-            onChange={onValueChange.bind('link_text')}
-            value={link_text}
-            tagName={'button'}
-            className={'btn btn-small btn-medium btn-primary hero__button'}
-            placeholder={'button text (optional)'}
-          />
-        </div>
-        <div style={{width: '400px'}}>
-          <RichText
-            onChange={onValueChange.bind('link_url')}
-            value={link_url}
+            onChange={onValueChange.bind('description')}
+            value={description}
             tagName={'p'}
-            className={''}
-            placeholder={'button url (optional unless button text is used)'}
-            style={{backgroundColor: 'white'}}
+            className={'hero__description'}
+            placeholder={'enter an abstract / description (optional)'}
           />
+          <div style={{width: '280px'}}>
+            <RichText
+              onChange={onValueChange.bind('link_text')}
+              value={link_text}
+              tagName={'button'}
+              className={'btn btn-small btn-medium btn-primary hero__button'}
+              placeholder={'button text (optional)'}
+            />
+          </div>
+          <div style={{width: '400px'}}>
+            <RichText
+              onChange={onValueChange.bind('link_url')}
+              value={link_url}
+              tagName={'p'}
+              className={''}
+              placeholder={'button url (optional unless button text is used)'}
+              style={{backgroundColor: 'white'}}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  ;
+    ;
 
-  const getImageOrButton = (openEvent) => {
-    if (image_id) {
-      return (
-        <div style={{
-          height: "100%",
-          overflow: "hidden",
-          backgroundImage: `url(${image_url})`,
-          backgroundSize: "cover"
-        }}>
+    const getImageOrButton = (openEvent) => {
+      if (image_id) {
+        return (
+          <div style={{
+            height: "100%",
+            overflow: "hidden",
+            backgroundImage: `url(${image_url})`,
+            backgroundSize: "cover"
+          }}>
             <BlockControls>
               <div className={'components-toolbar'}>
-                <button onClick={openEvent}>change hero image</button>
+                <a className={'components-toolbar-text-button'} onClick={openEvent}>change hero image</a>
               </div>
+
+
             </BlockControls>
-          {fields}
-        </div>
-      );
-    } else {
-      return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-          <Button
-            onClick={openEvent}
-            style={{position: "absolute", top: "50%", left: "50%", transform: 'translateX(-50%) translateY(-50%)'}}
-            className="btn btn-large btn-primary">
-            select an image
-          </Button>
-        </div>
-      );
-    }
-  };
+            {fields}
+          </div>
+        );
+      } else {
+        return (
+          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <Button
+              onClick={openEvent}
+              style={{position: "absolute", top: "50%", left: "50%", transform: 'translateX(-50%) translateY(-50%)'}}
+              className="btn btn-large btn-primary">
+              select an image
+            </Button>
+          </div>
+        );
+      }
+    };
 
     let heroClass = "hero";
     if (is_small === true) {
@@ -105,18 +110,20 @@ export default function HeroImageSelected(props) {
           />
         </MediaUploadCheck>
       </div>,
-        <InspectorControls>
-          <PanelBody title={'Height'}>
-            <ToggleControl
-              label={'small header'}
-              help={'When selected the header height will be smaller than normal. Also, the abstract / description text will no longer appear!'}
-              value={is_small}
-              checked={is_small}
-              onChange={onValueChange.bind('is_small')}
-            />
-          </PanelBody>
-        </InspectorControls>
+      <InspectorControls>
+        <PanelBody title={'Height'}>
+          <ToggleControl
+            label={'small header'}
+            help={'When selected the header height will be smaller than normal. Also, the abstract / description text will no longer appear!'}
+            value={is_small}
+            checked={is_small}
+            onChange={onValueChange.bind('is_small')}
+          />
+        </PanelBody>
+      </InspectorControls>
     ])
+  }
+
 }
 
 
