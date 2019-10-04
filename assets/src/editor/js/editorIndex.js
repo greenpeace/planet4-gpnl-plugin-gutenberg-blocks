a295ab617a66018e920b1f11c33dfaa3c38af41f
Dirk Faber <d.j.faber@outlook.com>
2019-10-04 16:34:46 +0200

    gitflow-feature-stash: liveblog

--- assets/src/editor/js/editorIndex.js
+++ assets/src/editor/js/editorIndex.js
@@ -2,28 +2,31 @@
 // Styles of the blocks are imported separately in the 'editor-styles.scss' file.
 
 import { QuoteBlock } from "../../blocks/Quote/js/QuoteBlock";
-const quoteBlock = new QuoteBlock();
+new QuoteBlock();
 
 import { HeroImageBlock } from "../../blocks/HeroImage/js/HeroImageBlock";
-const heroImageBlock = new HeroImageBlock();
+new HeroImageBlock();
 
 import { NewsletterBlock } from "../../blocks/Newsletter/js/NewsletterBlock";
-const newsletterBlock = new NewsletterBlock();
+new NewsletterBlock();
 
 import { PetitionBlock } from "../../blocks/Petition/js/PetitionBlock";
-const petitionBlock = new PetitionBlock();
+new PetitionBlock();
 
 import { DonationBlock } from "../../blocks/Donation/js/DonationBlock";
-const donationBlock = new DonationBlock();
+new DonationBlock();
 
 import { TwoColumnEmbedBlock } from "../../blocks/TwoColumnEmbed/js/TwoColumnEmbedBlock";
-const twoColumnEmbedBlock = new TwoColumnEmbedBlock();
+new TwoColumnEmbedBlock();
 
 import { NoindexBlock } from "../../blocks/Noindex/js/NoindexBlock";
-const noindexBlock = new NoindexBlock();
+new NoindexBlock();
 
 import { InforequestBlock } from "../../blocks/Inforequest/js/InforequestBlock";
-const inforequestBlock = new InforequestBlock();
+new InforequestBlock();
 
 import { EducationcoversBlock } from "../../blocks/Educationcovers/js/EducationcoversBlock";
-const educationcoversBlock = new EducationcoversBlock();
+new EducationcoversBlock();
+
+import { LiveblogBlock } from "../../blocks/Liveblog/js/LiveblogBlock";
+new LiveblogBlock();
