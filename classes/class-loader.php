811a29eb832cec8ed260f0ea135a89d3e21402f6
Dirk Faber <d.j.faber@outlook.com>
2019-10-24 13:42:59 +0200

    add bootstrap css as a css dependency for every asset

--- classes/class-loader.php
+++ classes/class-loader.php
@@ -309,9 +309,9 @@ final class Loader {
 	/**
 	 * Load assets for the frontend.
 	 */
-//	public function enqueue_public_assets() {
-//
-//	}
+	public function enqueue_public_assets() {
+		wp_enqueue_style( 'bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css', array(), '4.1.1' );
+	}
 
 	/**
 	 * Registers a new category for our blocks
