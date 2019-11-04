9ce0a3e6d63333ae1f0f8661fb70ebe680f5bf90
Oscar Keur <o.e.keur@gmail.com>
2019-11-04 14:53:35 +0100

    Fix opacity on newsletter block

--- assets/build/editorIndex.min.asset.php
+++ assets/build/editorIndex.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-editor', 'wp-element'), 'version' => '25118c2e5a14526eac1ef0ee30087470');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-editor', 'wp-element'), 'version' => '6e8abfdfc92c162bb9e5bb4b9ca74396');
