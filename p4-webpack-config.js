41947b58e15c83c82d96b8f12ca7e0bdd9f46ec5
Dirk Faber <d.j.faber@outlook.com>
2020-01-24 16:00:03 +0100

    Resolve  .js and .jsx files with webpack.

--- p4-webpack-config.js
+++ p4-webpack-config.js
@@ -62,6 +62,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+        resolve: { extensions: [".js", ".jsx"] },
         use: ['babel-loader']
       },
       {
