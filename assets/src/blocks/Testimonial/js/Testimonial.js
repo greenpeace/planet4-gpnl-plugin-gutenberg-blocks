268c9536bb5e397e9b7ca66bedaf022aacc3159a
Dirk Faber <d.j.faber@outlook.com>
2020-02-12 14:24:05 +0100

    Finalize testimonial block

--- assets/src/blocks/Testimonial/js/Testimonial.js
+++ assets/src/blocks/Testimonial/js/Testimonial.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {ServerSideRender} from '@wordpress/components';
+import {PanelBody, ServerSideRender, ToggleControl} from '@wordpress/components';
+import {RichText, MediaUpload, MediaUploadCheck, InspectorControls, BlockControls} from '@wordpress/editor';
 
-const {RichText, MediaUpload, MediaUploadCheck} = wp.editor;
 const {Button} = wp.components;
 
 export class Testimonial extends Component {
@@ -17,11 +17,16 @@ export class Testimonial extends Component {
     const getImageOrButton = (openEvent) => {
       if (image_id) {
         return (
-          <img
+          [<img
             src={image_url}
             onClick={openEvent}
             className="quote__img"
-          />
+          />,
+            <BlockControls>
+              <div className={'components-toolbar'}>
+                <a className={'components-toolbar-text-button'} onClick={openEvent}>verander afbeelding</a>
+              </div>
+            </BlockControls>]
         );
       } else {
         return (
@@ -36,7 +41,7 @@ export class Testimonial extends Component {
       }
     };
     return (
-      <div className="block block-testimonial">
+      <section className="block block-testimonial">
         <RichText
           onChange={handleValueChange.bind('title')}
           value={title}
@@ -46,23 +51,23 @@ export class Testimonial extends Component {
           placeholder="Vul een titel in."
           keepPlaceholderOnFocus
         />
-        <div className="col-4">
-        <MediaUploadCheck>
-          <MediaUpload
-            type="image"
-            onSelect={this.props.onSelectImage}
-            value={this.props.image_id}
-            render={({open}) => getImageOrButton(open)}
+        <div className="image-container" style={this.props.image_right === true ? {float: 'right'} : null}>
+          <MediaUploadCheck>
+            <MediaUpload
+              type="image"
+              onSelect={this.props.onSelectImage}
+              value={this.props.image_id}
+              render={({open}) => getImageOrButton(open)}
+            />
+          </MediaUploadCheck>
+          <RichText
+            onChange={handleValueChange.bind('name')}
+            value={name}
+            name="name"
+            tagName="p"
+            className="name"
+            placeholder="Vul de naam en eventueel de leeftijd in."
           />
-        </MediaUploadCheck>
-        <RichText
-          onChange={handleValueChange.bind('name')}
-          value={name}
-          name="name"
-          tagName="p"
-          className="name"
-          placeholder="Vul de naam en eventueel de leeftijd in."
-        />
         </div>
 
         <RichText
@@ -70,10 +75,23 @@ export class Testimonial extends Component {
           value={content}
           name="content"
           tagName="p"
-          className=""
+          className="content"
           placeholder="Vul de aanbeveling of ervaring in."
         />
-      </div>
+
+        <InspectorControls>
+          <PanelBody title={'Positie van de afbeelding'}>
+            <ToggleControl
+              label={'Afbeelding rechts'}
+              value={image_right}
+              checked={image_right}
+              onChange={handleValueChange.bind('image_right')}
+            />
+          </PanelBody>
+
+        </InspectorControls>
+
+      </section>
     );
   }
 
@@ -85,8 +103,10 @@ export class Testimonial extends Component {
           title: this.props.title,
           name: this.props.name,
           description: this.props.description,
+          content: this.props.content,
           image_url: this.props.image_url,
           image_id: this.props.image_id,
+          image_right: this.props.image_right,
         }}
       >
       </ServerSideRender>
