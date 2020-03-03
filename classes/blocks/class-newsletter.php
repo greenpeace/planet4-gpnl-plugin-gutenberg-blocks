cede108e36bf58e4b0a2546bb0d199d0d0017dc3
Oscar Keur <o.e.keur@gmail.com>
2020-03-03 15:26:07 +0100

    Move functions into class as methods

--- classes/blocks/class-newsletter.php
+++ classes/blocks/class-newsletter.php
@@ -72,6 +72,11 @@ class Newsletter extends Base_Block {
 
 		);
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_if_block_is_present' ] );
+
+		add_action( 'wp_ajax_newsletter_form_process', [ $this, 'newsletter_form_process' ] );
+		add_action( 'wp_ajax_nopriv_newsletter_form_process', [ $this, 'newsletter_form_process' ] );
+		add_action( 'wp_ajax_request_id', [ $this, 'request_id' ] );
+		add_action( 'wp_ajax_nopriv_request_id', [ $this, 'request_id' ] );
 	}
 
 	/**
@@ -124,120 +129,106 @@ class Newsletter extends Base_Block {
 		return $data;
 	}
 
+	public function newsletter_form_process() {
 
-}
+		$nonce        = htmlspecialchars( wp_strip_all_tags( $_POST['nonce'] ) );
+		$key_in_cache = wp_cache_get( $nonce, 'gpnl_cache' );
+		if ( ! $key_in_cache ) {
+			wp_send_json_error(
+				[
+					'statuscode' => 400,
+				],
+				500
+			);
+		}
+		wp_cache_delete( $nonce, 'gpnl_cache' );
 
-function newsletter_form_process() {
+		// get codes for processing in the database and sanitize
+		$marketingcode  = htmlspecialchars( wp_strip_all_tags( $_POST['marketingcode'] ) );
+		$literatuurcode = htmlspecialchars( wp_strip_all_tags( $_POST['literaturecode'] ) );
+		$screenid       = htmlspecialchars( wp_strip_all_tags( $_POST['screenid'] ) );
 
-	$nonce        = htmlspecialchars( wp_strip_all_tags( $_POST['nonce'] ) );
-	$key_in_cache = wp_cache_get( $nonce, 'gpnl_cache' );
-	if ( ! $key_in_cache ) {
-		wp_send_json_error(
+		// Get and sanitize the formdata
+		$naam  = wp_strip_all_tags( $_POST['name'] );
+		$email = wp_strip_all_tags( $_POST['mail'] );
+		$human = wp_strip_all_tags( $_POST['human'] );
+
+		if ( '' !== $human ) {
+			wp_send_json_error(
+				[
+					'statuscode' => 400,
+					// 'cUrlresult'    => $result,
+				],
+				500
+			);
+		}
+
+		$data_array = [
+			'voornaam'       => $naam,
+			'email'          => $email,
+			'marketingcode'  => $marketingcode,
+			'literatuurcode' => $literatuurcode,
+			'screenId'       => $screenid,
+		];
+
+		$data = wp_json_encode( $data_array );
+
+		$url = 'https://www.mygreenpeace.nl/GPN.RegistrerenApi/register/email';
+
+		// initiate a cUrl request to the database
+		$request = curl_init( $url );
+		curl_setopt( $request, CURLOPT_POSTFIELDS, $data );
+		curl_setopt( $request, CURLOPT_CUSTOMREQUEST, 'POST' );
+		curl_setopt( $request, CURLOPT_HEADER, true );
+		curl_setopt(
+			$request,
+			CURLOPT_HTTPHEADER,
 			[
-				'statuscode' => 400,
-			],
-			500
+				'Content-Type:application/json',
+				'Content-Length: ' . strlen( $data ),
+			]
 		);
-	}
-	wp_cache_delete( $nonce, 'gpnl_cache' );
+		curl_setopt( $request, CURLOPT_RETURNTRANSFER, true );
 
-	// get codes for processing in the database and sanitize
-	$marketingcode  = htmlspecialchars( wp_strip_all_tags( $_POST['marketingcode'] ) );
-	$literatuurcode = htmlspecialchars( wp_strip_all_tags( $_POST['literaturecode'] ) );
-	$screenid       = htmlspecialchars( wp_strip_all_tags( $_POST['screenid'] ) );
+		$result   = curl_exec( $request );
+		$httpcode = curl_getinfo( $request, CURLINFO_HTTP_CODE );
+		curl_close( $request );
 
-	// Get and sanitize the formdata
-	$naam  = wp_strip_all_tags( $_POST['name'] );
-	$email = wp_strip_all_tags( $_POST['mail'] );
-	$human = wp_strip_all_tags( $_POST['human'] );
-
-	if ( '' !== $human ) {
-		wp_send_json_error(
-			[
-				'statuscode' => 400,
-				// 'cUrlresult'    => $result,
-			],
-			500
-		);
-	}
-
-	$data_array = [
-		'voornaam'       => $naam,
-		'email'          => $email,
-		'marketingcode'  => $marketingcode,
-		'literatuurcode' => $literatuurcode,
-		'screenId'       => $screenid,
-	];
-
-	$data = wp_json_encode( $data_array );
-
-	$url = 'https://www.mygreenpeace.nl/GPN.RegistrerenApi/register/email';
-
-	// initiate a cUrl request to the database
-	$request = curl_init( $url );
-	curl_setopt( $request, CURLOPT_POSTFIELDS, $data );
-	curl_setopt( $request, CURLOPT_CUSTOMREQUEST, 'POST' );
-	curl_setopt( $request, CURLOPT_HEADER, true );
-	curl_setopt(
-		$request,
-		CURLOPT_HTTPHEADER,
-		[
-			'Content-Type:application/json',
-			'Content-Length: ' . strlen( $data ),
-		]
-	);
-	curl_setopt( $request, CURLOPT_RETURNTRANSFER, true );
-
-	$result   = curl_exec( $request );
-	$httpcode = curl_getinfo( $request, CURLINFO_HTTP_CODE );
-	curl_close( $request );
-
-	// Give the appropriate response to the frontend
-	if ( false === $result || 200 !== $httpcode ) {
-		wp_send_json_error(
+		// Give the appropriate response to the frontend
+		if ( false === $result || 200 !== $httpcode ) {
+			wp_send_json_error(
+				[
+					'statuscode' => $httpcode,
+				],
+				500
+			);
+		}
+		wp_send_json_success(
 			[
 				'statuscode' => $httpcode,
 			],
-			500
+			200
 		);
 	}
-	wp_send_json_success(
-		[
-			'statuscode' => $httpcode,
-		],
-		200
-	);
-}
 
-// use this version for if you want the callback to work for users who are logged in.
-add_action( 'wp_ajax_newsletter_form_process', 'P4NL_GB_BKS\Blocks\newsletter_form_process' );
-// use this version for if you want the callback to work for users who are not logged in.
-add_action( 'wp_ajax_nopriv_newsletter_form_process', 'P4NL_GB_BKS\Blocks\newsletter_form_process' );
+	public function request_id() {
 
-
-function request_id() {
-
-	// Generate an id and try to get it from cache.
-	$unique_id       = hexdec( bin2hex( openssl_random_pseudo_bytes( 2 ) ) );
-	$key_unavailable = wp_cache_get( $unique_id, 'gpnl_cache' );
-
-	// If it's already in the cache, keep on trying to find a open position.
-	while ( $key_unavailable ) {
+		// Generate an id and try to get it from cache.
 		$unique_id       = hexdec( bin2hex( openssl_random_pseudo_bytes( 2 ) ) );
-		$key_unavailable = wp_cache_get( $unique_id, 'gpnl_cache', true );
+		$key_unavailable = wp_cache_get( $unique_id, 'gpnl_cache' );
+
+		// If it's already in the cache, keep on trying to find a open position.
+		while ( $key_unavailable ) {
+			$unique_id       = hexdec( bin2hex( openssl_random_pseudo_bytes( 2 ) ) );
+			$key_unavailable = wp_cache_get( $unique_id, 'gpnl_cache', true );
+		}
+		wp_cache_add( $unique_id, true, 'gpnl_cache', 300 );
+
+		wp_send_json_success(
+			[
+				'nonce' => $unique_id,
+			],
+			200
+		);
 	}
-	wp_cache_add( $unique_id, true, 'gpnl_cache', 300 );
-
-	wp_send_json_success(
-		[
-			'nonce' => $unique_id,
-		],
-		200
-	);
 }
-
-// TODO make reference to function as : [ $this, 'function']
-// use this version for if you want the callback to work for users who are logged in
-add_action( 'wp_ajax_request_id', 'P4NL_GB_BKS\Blocks\request_id' );
-// use this version for if you want the callback to work for users who are not logged in
-add_action( 'wp_ajax_nopriv_request_id', 'P4NL_GB_BKS\Blocks\request_id' );
