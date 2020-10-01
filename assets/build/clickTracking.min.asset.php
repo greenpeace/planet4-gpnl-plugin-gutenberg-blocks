c7b9810bc230ce60d038e14634fe833c980fc690
Oscar Keur <o.e.keur@gmail.com>
2020-10-01 15:07:21 +0200

    Add build files

--- assets/build/clickTracking.min.asset.php
+++ assets/build/clickTracking.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '2f2f8249f57a415196eab4346f3485fb');
+<?php return array('dependencies' => array('jquery'), 'version' => '77031e45cb5b2344866a35ba2f3e9566');
