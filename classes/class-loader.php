ee6112578d23ea298ca427fa654a0aab3bb5a915
Oscar Keur <o.e.keur@gmail.com>
2020-12-07 14:04:55 +0100

    Remove newsletter block

--- classes/class-loader.php
+++ classes/class-loader.php
@@ -99,7 +99,6 @@ final class Loader {
 		$this->blocks = [
 			new Blocks\Quote(),
 			new Blocks\HeroImage(),
-//			new Blocks\Newsletter(),
 			new Blocks\TwoColumnEmbed(),
 			new Blocks\Inforequest(),
 			new Blocks\Noindex(),
