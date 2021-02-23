0990d7c88dd707928b234f0133559674f0479a77
Oscar Keur <o.e.keur@gmail.com>
2021-02-23 11:47:42 +0100

    refactor: add missing export statement

--- assets/src/components/forms/SelectGroup.jsx
+++ assets/src/components/forms/SelectGroup.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import SelectOption from './SelectOption';
 
- default class SelectGroup extends Component {
+export default class SelectGroup extends Component {
   constructor(props) {
     super(props);
 
