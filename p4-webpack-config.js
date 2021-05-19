5d9bdfcfef9d8cb59a2e063c6ebbf20a2cbbbed4
Oscar Keur <o.e.keur@gmail.com>
2021-05-19 17:07:33 +0200

    refactor: extract entries to separate file

--- p4-webpack-config.js
+++ p4-webpack-config.js
@@ -1,48 +1,14 @@
 const defaultConfig = require('./node_modules/@wordpress/scripts/config/webpack.config');    // Require default Webpack config
+const entries = require('./webpack/entries');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
-const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   ...defaultConfig,
-  entry: {
-
-    columns: path.resolve(__dirname, './assets/src/blocks/Columns/render.jsx' ),
-    socialMessage: path.resolve(__dirname, './assets/src/blocks/SocialMessage/render.jsx' ),
-
-    // assets for the editor (backend)
-    editorIndex: path.resolve(__dirname, './assets/src/editor/js/editorIndex.js' ),
-    clickTracking: path.resolve(__dirname, './assets/src/editor/js/Sidebar/clickTracking.js' ),
-    'editor-style': path.resolve(__dirname, './assets/src/editor/scss/editor-styles.scss' ),
-    eActivismCounter: './assets/src/editor/js/Sidebar/clickTracking.js',
-
-
-    // assets for the educationcovers block
-    educationcoversHelper: path.resolve(__dirname, './assets/src/blocks/Educationcovers/js/educationcoversHelper.js' ),
-    educationcovers: path.resolve(__dirname, './assets/src/blocks/Educationcovers/scss/educationcovers.scss' ),
-
-    // assets for the inforequest block
-    inforequestHelper: path.resolve(__dirname, './assets/src/blocks/Inforequest/js/InforequestHelper.js' ),
-
-    heroImageRendering: path.resolve(__dirname, './assets/src/blocks/HeroImage/js/heroImageRendering.js' ),
-
-    // Separate css files are generated for the blocks so they can be used only when required on public pages.
-    'hero-image': path.resolve(__dirname, './assets/src/blocks/HeroImage/scss/hero-image.scss' ),
-    quote: path.resolve(__dirname, './assets/src/blocks/Quote/scss/quote.scss' ),
-
-    // Assets for the collapsible block.
-    collapsible: path.resolve(__dirname, './assets/src/blocks/Collapsible/scss/collapsible.scss' ),
-
-    testimonial: path.resolve(__dirname, './assets/src/blocks/Testimonial/scss/testimonial.scss' ),
-
-    modal: path.resolve(__dirname, './assets/src/blocks/Modal/styles/frontend.scss' ),
-    'pdf-embed': path.resolve(__dirname, './assets/src/blocks/PdfEmbed/styles/public.scss' ),
-    'profile-picture-overlay': path.resolve(__dirname, './assets/src/blocks/ProfilePictureOverlay/publicIndex.js' ),
-
-  },
+  entry: entries,
   output: {
     filename: '[name].min.js',
     path: __dirname + '/assets/build'
